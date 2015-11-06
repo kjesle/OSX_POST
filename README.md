6c06a66ce34bdf8c8d086b39db2308365d6c691c
Kjetil Sletten <kjsletten@gmail.com>
2015-11-06 17:05:36 +0100

    Added some OSX specific

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 #OSX Post-install
 
-dotfiles, settings etc.
+This repo contains different settings and dot-files that should be installed after a clean install of OSX.
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/usr/local/Caskroom"
+##dot-files
+
+##Sublime files
+Settings for sublime, snippets, Latexing-plugin settings, etc.
+Environment settings -> i.e do not show .aux files in Sublime project for a latex environment.
+
+##Webstorm
+Snippets for Webstorm
--- post-install.sh
+++ post-install.sh
@@ -1,8 +1,8 @@
 ## SET HOSTNAME IN ALL THE RIGHT PLACES
-sudo scutil --set ComputerName "MACHINE_NAME"
-sudo scutil --set HostName "MACHINE_NAME"
-sudo scutil --set LocalHostName "MACHINE_NAME"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "MACHINE_NAME"
+sudo scutil --set ComputerName "ComputerName"
+sudo scutil --set HostName "ComputerName"
+sudo scutil --set LocalHostName "ComputerName"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "ComputerName"
 
 ## INSTALL XCODE
 xcode-select --install
@@ -12,17 +12,17 @@ read -p "Press any key to continue." -n1 -s
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ### CLI tools
+brew update
+
 brew install caskroom/cask/brew-cask
-brew install curl
-brew install Caskroom/cask/java
 brew install gcc
 brew install git
-brew install nodejs
-brew install scala
-brew install the_silver_searcher
 brew install tree
 brew install unrar
 brew install wget
+brew install Caskroom/cask/java
+
+
 
 ### Cask
 brew cask install 1password
@@ -47,10 +47,7 @@ brew cask install virtualbox
 brew cask install vagrant
 brew cask install macvim
 brew cask install vlc
-brew cask install webstorm
-brew cask install intellij-idea
 brew cask install atom
-brew tap caskroom/versions
 brew cask install sublime-text3
 
 
@@ -61,3 +58,25 @@ sudo softwareupdate --install -all
 
 ## Meteor
 curl https://install.meteor.com/ | sh
+
+echo ""
+echo "Disabling press-and-hold for keys in favor of a key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+echo ""
+echo "Setting a blazingly fast keyboard repeat rate (ain't nobody got time fo special chars while coding!)"
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+echo ""
+echo "Disabling auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+echo ""
+echo "Enabling the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+echo ""
+echo "Disable the sudden motion sensor as itâ€™s not useful for SSDs"
+sudo pmset -a sms 0
